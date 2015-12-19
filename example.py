cb45f99bf05479f374c391f118c5e1ac9e4d4966
Kevin Schiroo <kjschiroo@gmail.com>
2015-12-19 00:27:31 -0600

    removing dead code from example.py

--- example.py
+++ example.py
@@ -11,13 +11,7 @@ from wikichatter import TextBlocks as tb
 from wikichatter import IndentTree as indT
 from wikichatter import TalkPageParser
 
-def switch_marker(block_marker = ""):
-    if block_marker == "#### ":
-        return "     "
-    else:
-        return "#### "
-
-talk_samples_base = "../talk_samples/"
+talk_samples_base = "./talk_samples/"
 talk_files = []
 for (name, directories, files) in os.walk(talk_samples_base):
     talk_files.extend([name + "/" +f for f in files])
@@ -26,7 +20,7 @@ for (name, directories, files) in os.walk(talk_samples_base):
 
 sig_focused = True
 for f_path in talk_files:
-    print("\n"*3 + f_path + "\n"*3)
+    print("\n"*3 + f_path)
     with open(f_path, "r") as f:
         text = f.read()
         indent_tree_root = TalkPageParser.parse(text)
