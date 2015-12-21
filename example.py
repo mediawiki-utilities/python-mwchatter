50bf6d820b5d6a3e12a436ca6de02dd09e888219
Kevin Schiroo <kjschiroo@gmail.com>
2015-12-21 13:59:19 -0600

    cleaning up example

--- example.py
+++ example.py
@@ -1,24 +1,11 @@
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-
-import re
 import os
-from wikichatter import SignatureUtils
-from wikichatter import WikiIndentUtils
-import mwparserfromhell as mwp
-from wikichatter import WikiComments as wc
-from wikichatter import TextBlocks as tb
-from wikichatter import IndentTree as indT
 from wikichatter import TalkPageParser
 
 talk_samples_base = "./talk_samples/"
 talk_files = []
 for (name, directories, files) in os.walk(talk_samples_base):
-    talk_files.extend([name + "/" +f for f in files])
+    talk_files.extend([name + "/" + f for f in files])
 
-
-
-sig_focused = True
 for f_path in talk_files:
     print("\n"*3 + f_path)
     with open(f_path, "r") as f:
