99229ed445ebfbe9fa8618b725439256a0ce839c
Kevin Schiroo <kjschiroo@gmail.com>
2016-01-03 21:19:35 -0600

    added additional tests

--- test/test_talkpageparser.py
+++ test/test_talkpageparser.py
@@ -5,11 +5,11 @@ from test.schema import verify
 
 SECTION1 = "= Title ="
 SECTION2 = "== Title =="
-LEVEL0 = "Text"
-LEVEL1 = ":Text"
-LEVEL2 = "::Text"
-LEVEL3 = ":::Text"
-LEVEL4 = "::::Text"
+LEVEL0 = "Text0"
+LEVEL1 = ":Text1"
+LEVEL2 = "::Text2"
+LEVEL3 = ":::Text3"
+LEVEL4 = "::::Text4"
 SIGNATURE = " [[User:Name|Name]] ([[User talk:Name|talk]]) 19:40, 18 September 2013 (UTC)"
 OUTDENT = "{{outdent}}"
 EL = "\n"
@@ -35,3 +35,71 @@ class TalkPageParser(unittest.TestCase):
         self.assertEqual(len(output['sections']), 1)
         self.assertEqual(len(output['sections'][0]['comments']), 1)
         self.assertEqual(len(output['sections'][0]['comments'][0]['comments']), 1)
+
+    def test_outdent_parse(self):
+        text = "\n".join(
+            [
+                SECTION1,
+                LEVEL0,
+                LEVEL0 + SIGNATURE,
+                LEVEL1,
+                LEVEL1 + SIGNATURE,
+                LEVEL2 + SIGNATURE,
+                OUTDENT + LEVEL0 + SIGNATURE
+            ]
+        )
+
+        output = tpp.parse(text)
+
+        self.assertTrue(verify(output))
+        self.assertEqual(len(output['sections']), 1)
+        self.assertEqual(len(output['sections'][0]['comments']), 1)
+        self.assertEqual(len(output['sections'][0]['comments'][0]['comments']), 1)
+        self.assertEqual(len(output['sections'][0]
+                                   ['comments'][0]
+                                   ['comments'][0]
+                                   ['comments']), 1)
+        self.assertEqual(len(output['sections'][0]
+                                   ['comments'][0]
+                                   ['comments'][0]
+                                   ['comments'][0]
+                                   ['comments']), 1)
+
+    def test_multi_sections(self):
+        text = "\n".join(
+            [
+                SECTION1,
+                LEVEL0,
+                LEVEL0 + SIGNATURE,
+                SECTION1,
+                LEVEL0,
+                LEVEL0 + SIGNATURE,
+            ]
+        )
+
+        output = tpp.parse(text)
+
+        self.assertTrue(verify(output))
+        self.assertEqual(len(output['sections']), 2)
+        self.assertEqual(len(output['sections'][0]['comments']), 1)
+        self.assertEqual(len(output['sections'][1]['comments']), 1)
+
+    def test_subsections(self):
+        text = "\n".join(
+            [
+                SECTION1,
+                LEVEL0,
+                LEVEL0 + SIGNATURE,
+                SECTION2,
+                LEVEL0,
+                LEVEL0 + SIGNATURE,
+            ]
+        )
+
+        output = tpp.parse(text)
+
+        self.assertTrue(verify(output))
+        self.assertEqual(len(output['sections']), 1)
+        self.assertEqual(len(output['sections'][0]['comments']), 1)
+        self.assertEqual(len(output['sections'][0]['subsections']), 1)
+        self.assertEqual(len(output['sections'][0]['subsections'][0]['comments']), 1)
