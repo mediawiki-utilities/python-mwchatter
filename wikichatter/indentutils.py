3ddad166931d5350c04cee5f94f313a9b591b575
Kevin Schiroo <kjschiroo@gmail.com>
2016-01-14 22:10:35 -0600

    Moved indentutils to taking wikicode inputs

--- wikichatter/indentutils.py
+++ wikichatter/indentutils.py
@@ -1,33 +1,86 @@
 import mwparserfromhell as mwp
+import time
 
 
-def extract_indent_blocks(text):
+# Unclean code
+def extract_indent_blocks(wikicode):
     old_indent = 0
-    block_list = []
-    cur_block_lines = []
-    for line in text.split('\n'):
-        indent = find_line_indent(line)
+    wc_block_list = []
+    cur_block_wc_lines = []
+    for wc_line in _split_wikicode_on_endlines(wikicode):
+        line = str(wc_line)
+        indent = _find_line_indent(line)
         if indent != old_indent and line.strip() != "":
-            block = "\n".join(cur_block_lines)
+            wc_block = _join_wikicode(cur_block_wc_lines)
+            block = str(wc_block)
             if block.strip() != "":
-                block_list.append(block)
-            cur_block_lines = []
+                wc_block_list.append(wc_block)
+            cur_block_wc_lines = []
             old_indent = indent
-        cur_block_lines.append(line)
-    block = "\n".join(cur_block_lines)
+        cur_block_wc_lines.append(wc_line)
+    wc_block = _join_wikicode(cur_block_wc_lines)
+    block = str(wc_block)
     if block.strip() != "":
-        block_list.append(block)
-    return block_list
+        wc_block_list.append(block)
+    return wc_block_list
 
 
-def find_min_indent(text):
+# Unclean code
+def _split_wikicode_on_endlines(wikicode):
+    divided = []
+    cur = []
+    for node in wikicode.nodes:
+        if type(node) is mwp.nodes.text.Text:
+            split_nodes = _split_text_node_on_endline(node)
+            for sn in split_nodes:
+                cur.append(sn)
+                if "\n" in sn.value:
+                    divided.append(mwp.wikicode.Wikicode(cur))
+                    cur = []
+        else:
+            cur.append(node)
+    if len(cur) > 0:
+        divided.append(mwp.wikicode.Wikicode(cur))
+    return divided
+
+
+def _split_text_node_on_endline(text_node):
+    text = text_node.value
+    lines = _split_text_and_leave_delimiter(text, "\n")
+    results = []
+    for line in lines:
+        if line != "":
+            results.append(mwp.nodes.text.Text(line))
+    return results
+
+
+def _split_text_and_leave_delimiter(text, delimiter):
+    result = []
+    lines = text.split(delimiter)
+    for i, line in enumerate(lines):
+        if i == (len(lines) - 1):
+            break
+        result.append(line + delimiter)
+    result.append(lines[i])
+    return result
+
+
+def _join_wikicode(wikicode_list):
+    nodes = []
+    for wc in wikicode_list:
+        nodes.extend(wc.nodes)
+    return mwp.wikicode.Wikicode(nodes)
+
+
+def find_min_indent(wikicode):
+    text = str(wikicode)
     lines = text.split('\n')
     non_empty = [line for line in lines if line.strip() != ""]
-    indents = [find_line_indent(line) for line in non_empty]
+    indents = [_find_line_indent(line) for line in non_empty]
     return min(indents)
 
 
-def find_line_indent(line):
+def _find_line_indent(line):
     return _count_indent_in_some_order(line)
 
 
@@ -53,8 +106,7 @@ def _count_leading_char(line, char):
         return 1 + _count_leading_char(line[1:], char)
 
 
-def has_continuation_indent(text):
-    wikicode = mwp.parse(text, skip_style_tags=True)
+def has_continuation_indent(wikicode):
     templates = wikicode.filter_templates()
     if len(templates) > 0:
         potential_cont = str(templates[0])
