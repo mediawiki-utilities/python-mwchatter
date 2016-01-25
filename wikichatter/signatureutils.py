768fea0e537fafa14b75829f233cf512b612fce4
Kevin Schiroo <kjschiroo@gmail.com>
2016-01-24 21:04:25 -0600

    shift signature utils to wikicode

--- wikichatter/signatureutils.py
+++ wikichatter/signatureutils.py
@@ -1,4 +1,5 @@
 import re
+import mwparserfromhell as mwp
 
 
 class Error(Exception):
@@ -23,7 +24,7 @@ USER_TALK_RE = re.compile(r"(\[\[\W*user[_ ]talk\W*:(.*?)\|[^\]]+\]\])", re.I)
 USER_CONTRIBS_RE = re.compile(r"(\[\[\W*Special:Contributions/(.*?)\|[^\]]+\]\])", re.I)
 
 
-def extract_signatures(text):
+def extract_signatures(wcode):
     """
     Returns all signatures found in the text as a list of dictionaries
     [
@@ -34,26 +35,81 @@ def extract_signatures(text):
          'end':<end_location>}
     ]
     """
+    nodes = wcode.nodes
     signature_list = []
-    divided_text = _divide_on_timestamp(text)
-    for (t, base_start, _) in divided_text:
-        s = _try_extract_signature(t)
-        if s is not None:
-            s['start'] += base_start
-            s['end'] += base_start
-            signature_list.append(s)
+    signature_loc = _find_signatures_in_nodes(nodes)
+    for (start, end) in signature_loc:
+        signature_list.append(mwp.wikicode.Wikicode(nodes[start:end + 1]))
     return signature_list
 
 
-def _divide_on_timestamp(text):
-    divided_text = []
-    locations = _find_timestamp_locations(text)
-    old_end = 0
-    for _, t_end in locations:
-        end = _find_next_endline(text, t_end)
-        divided_text.append((text[old_end:end], old_end, end))
-        old_end = end
-    return divided_text
+def _divide_wikicode_on_timestamps(wcode):
+    nodes = wcode.nodes
+    divided_nodes = _divide_nodes_on_timestamp(nodes)
+    return [mwp.wikicode.Wikicode(ns) for ns in divided_nodes]
+
+
+def _divide_nodes_on_timestamp(nodes):
+    divided = []
+    locations = _find_timestamp_locations(nodes)
+    start = 0
+    for loc in locations:
+        end = loc + 1
+        cur = nodes[start:end]
+        divided.append(cur)
+        start = end
+    if end > len(nodes):
+        last = nodes[start:]
+        divided.append(last)
+    return divided
+
+
+def _find_signatures_in_nodes(nodes):
+    result = []
+    candidate_locations = _find_timestamp_locations(nodes)
+    for loc in candidate_locations:
+        if "00:47, 30 April 2012" in str(nodes[loc]):
+            wcode = mwp.wikicode.Wikicode([nodes[loc]])
+            links = [l for l in wcode.filter_wikilinks()]
+            import pdb; pdb.set_trace()
+        start, end = _find_signature_near_timestamp(loc, nodes)
+        if start is not None:
+            result.append((start, end))
+    return result
+
+
+def _find_signature_near_timestamp(timestamp_loc, nodes):
+    end = timestamp_loc
+    start = _find_start_of_signature_ending_at(end, nodes)
+    if start is None:
+        end = end + 1
+        if len(nodes) > end:
+            start = _find_start_of_signature_ending_at(end, nodes)
+    if start is None:
+        end = None
+    return start, end
+
+
+def _find_start_of_signature_ending_at(end, nodes):
+    start = None
+    found_user = False
+    found_date = False
+    for i in range(0, 5):
+        cur = end - i
+        if cur < 0 or cur >= len(nodes):
+            break
+        node = nodes[cur]
+        n_has_un = _node_contains_username(node)
+        n_has_ts = _node_has_timestamp(node)
+        found_user = found_user or n_has_un
+        found_date = found_date or n_has_ts
+        if n_has_un or n_has_ts:
+            start = cur
+        elif len(str(node)) > 5 and (found_user and found_date):
+            break
+    if not (found_user and found_date):
+        start = None
+    return start
 
 
 def _find_next_endline(text, position):
@@ -88,6 +144,7 @@ def _extract_rightmost_signature(text):
 
 
 def _extract_rightmost_timestamp(text):
+    # TODO find_timestamp_locations has changed
     ts_loc = _find_timestamp_locations(text)
     if len(ts_loc) == 0:
         raise NoTimestampError(text)
@@ -122,7 +179,6 @@ def _extract_timestamp_user(text):
 
 def _extract_userpage_user(text):
     up = USER_RE.match(text)
-    # import pdb; pdb.set_trace()
     if up is None:
         raise NoUsernameError(text)
     raw_username = up.group(2)
@@ -151,8 +207,47 @@ def _clean_extracted_username(raw_username):
     return username.strip()
 
 
-def _find_timestamp_locations(text):
-    return _find_regex_locations(TIMESTAMP_RE, text)
+def _find_timestamp_locations(nodes):
+    locations = []
+    for i, node in enumerate(nodes):
+        if _node_has_timestamp(node):
+            locations.append(i)
+    return locations
+
+
+def _node_has_timestamp(node):
+    return _node_matches_regex(node, TIMESTAMP_RE)
+
+
+def _node_is_part_of_signature(node):
+    return (_node_contains_username(node) or
+            _node_has_timestamp(node))
+
+
+def _node_contains_username(node):
+    wcode = mwp.wikicode.Wikicode([node])
+    links = [l for l in wcode.filter_wikilinks()]
+    for link in links:
+        if (_node_is_usertalk(link) or _node_is_userpage(link) or _node_is_usercontribs(link)):
+            return True
+    return False
+
+
+def _node_is_usertalk(node):
+    return _node_matches_regex(node, USER_TALK_RE)
+
+
+def _node_is_userpage(node):
+    return _node_matches_regex(node, USER_RE)
+
+
+def _node_is_usercontribs(node):
+    return _node_matches_regex(node, USER_CONTRIBS_RE)
+
+
+def _node_matches_regex(node, regex):
+    text = str(node)
+    return re.search(regex, text) is not None
 
 
 def _find_userpage_locations(text):
