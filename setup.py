f99d7ab5477814b84b71c3e6beef2d368b292c75
Kevin Schiroo <kjschiroo@gmail.com>
2016-04-06 11:36:42 -0500

    removing unneeded packages

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ setup(
     version='0.1',
     description='Parser for MediaWiki talk pages',
     url='https://github.com/kjschiroo/WikiChatter',
+
     author='Kevin Schiroo',
     author_email='kjschiroo@gmail.com',
     license='MIT',
+
     packages=['wikichatter'],
+    install_requires=['mwparserfromhell']
 )
