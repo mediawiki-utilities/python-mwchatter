0e07f4609fec2cb764f50e871222377fcfd0767d
Kevin Schiroo <kjschiroo@gmail.com>
2017-02-15 22:04:02 -0600

    update version since change may break code

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='WikiChatter',
-    version='0.2.3',
+    version='0.3.0',
     description='Parser for MediaWiki talk pages',
     url='https://github.com/kjschiroo/WikiChatter',
 
